c3cf508c0b2c48b6d821036bd36a58fad7a6dd49
Petteri Huvio <petteri.huvio@gmail.com>
2023-03-26 15:18:33 +0300

    fix SQLschema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
-    name VARCHAR
+    name VARCHAR (50)
 );
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR (50) UNIQUE,
+    username VARCHAR (20) UNIQUE,
     password VARCHAR (50),
     is_admin BOOLEAN
-)
+);
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
@@ -16,21 +16,21 @@ CREATE TABLE comments (
     user_id INTEGER REFERENCES users,
     stars INTEGER,
     comment VARCHAR (500)
-)
+);
 
 CREATE TABLE restaurantinformation (
     id SERIAL PRIMARY KEY,
     restaurant_id INTEGER REFERENCES restaurants,
     key VARCHAR (50) UNIQUE,
     value VARCHAR (500)
-)
+);
 
 CREATE TABLE groups (
     id SERIAL PRIMARY KEY,
     name VARCHAR (50) UNIQUE
-)
+);
 
 CREATE TABLE restaurantsingroups (
     group_id INTEGER REFERENCES groups,
     restaurant_id INTEGER REFERENCES restaurants
-)
+);
