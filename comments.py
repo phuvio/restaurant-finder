51adbc24c12b22a86d27f600df1973a7bfd90089
Petteri Huvio <petteri.huvio@gmail.com>
2023-03-30 17:44:53 +0300

    add comments.py
    SQL queries for comments

---
+++ comments.py
@@ -0,0 +1,17 @@
+from db import db
+from sqlalchemy.sql import text
+
+
+def add_comment(restaurant_id, user_id, stars, comment):
+    sql = text("""INSERT INTO comments
+                  (restaurant_id, user_id, stars, comment)
+                  VALUES 
+                  (:restaurant_id, :user_id, :stars, :comment)""")
+    db.session.execute(sql, {"restraurant_id":restaurant_id, "user_id":user_id, "stars":stars, "comment":comment})
+    db.session.commit()
+
+def remove_comment(comment_id):
+    sql = text("""DELETE FROM comments
+                  WHERE comment_id=:comment_id""")
+    db.session.execute(sql, {"comment_id":comment_id})
+    db.session.commit()
