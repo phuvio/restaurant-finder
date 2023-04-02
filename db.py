0aa9fb44b53bdcb8279a854ac7e95c804abb4410
Petteri Huvio <petteri.huvio@gmail.com>
2023-04-02 19:50:49 +0300

    fix db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
