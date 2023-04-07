ca7cd7d66e6d845dec43761ba08fcbe91c127955
Petteri Huvio <petteri.huvio@gmail.com>
2023-04-07 16:46:32 +0300

    add links to map markers

--- map.py
+++ map.py
@@ -5,18 +5,25 @@ import restaurants
 def create_map():
     all_restaurants = restaurants.get_all_restaurants()
 
-    markers = [{"lat": x.latitude,
-                "lng": x.longitude,
-                "label": x.name}
-                for x in all_restaurants]
+    markers = []
+    
+    for restaurant in all_restaurants:
+        marker = {}
+        marker["lat"] = restaurant.latitude
+        marker["lng"] = restaurant.longitude
+        marker["label"] = restaurant.name
+        link = str(restaurant.avg_stars) + " <a href=/restaurant/" + str(restaurant.id) + ">" + restaurant.name + "</a>"
+        marker["infobox"] = link
+        markers.append(marker)
     
     mymap = Map(
         identifier="mymap",
         lat=60.169857,
         lng=24.938379,
         markers=markers,
-        zoom=13,
-        zoom_control=True
+        zoom=14,
+        zoom_control=True,
+        style="height:400px;width:600px;margin:0;",
     )
 
     return mymap
